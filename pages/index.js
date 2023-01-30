86ae906964eaca38041cf4fa576ebe661692b036
Alex Whiteside <1505496+alexw23@users.noreply.github.com>
2023-01-31 09:30:02 +1100

    Bring up to speed with NextJS V13, React V18 and Latest Flickity

--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Flickity from "react-flickity-component";
 
-import "../styles.css";
-import "flickity/css/flickity.css";
-
 function Carousel() {
   return (
     <Flickity>
-      <img style={{ marginLeft: 5, marginRight: 5 }} src="https://placeimg.com/640/480/animals" />
-      <img style={{ marginLeft: 5, marginRight: 5 }} src="https://placeimg.com/640/480/animals" />
+      <img
+        style={{ marginLeft: 5, marginRight: 5 }}
+        src="https://placeimg.com/640/480/animals"
+      />
+      <img
+        style={{ marginLeft: 5, marginRight: 5 }}
+        src="https://placeimg.com/640/480/animals"
+      />
     </Flickity>
   );
 }
 
-function Home() {
+export default function Home() {
   return (
-    <div className="App">
+    <div className={styles.container}>
       <h1>React Flickity</h1>
       <Carousel />
     </div>
   );
 }
-
-export default Home
